fc36dfec1c1bc720bf1746880bbc7dfcf568f0b5
Roman Linkov <roman.linkov@vk.com>
2023-03-10 09:51:24 +0300

    62 для 4х4 работает

--- 62/Program.cs
+++ 62/Program.cs
@@ -31,29 +31,27 @@ void SpringFill2DimIntArray(int[,] array2Dim)
     int i = 2;
     while (i <= lngth)
     {
-    
-        if (direction==0 & col+1 < array2Dim.GetLength(1)) col++;
-        if (direction==1 & row+1 < array2Dim.GetLength(0)) row++;
-        if (direction==2 & col-1 >=0) col=col-1;
-        if (direction==3 & row-1 >=0) row=row-1;
-
-        if (row<array2Dim.GetLength(0) & col<array2Dim.GetLength(1) & row>=0 & col>=0)
-        {
-            if (array2Dim[row,col]==0) 
+        if (array2Dim[row,col]==0) 
             {
                 array2Dim[row,col]=i;
-                Console.WriteLine($"{row}, {col} -> {i}");
+                Console.WriteLine($"{row}, {col} -> {i} -> dir {direction}");
                 i++;
+                //Console.WriteLine($"{direction}");
             }
-           else direction=ChooseDirection(array2Dim, row, col); // Console.WriteLine($"ТУТ");
-        }
-        else direction=ChooseDirection(array2Dim, row, col); // Console.WriteLine($"здесь  123");
-    }   i++;
+        //else direction=ChooseDirection(array2Dim, row, col); // Console.WriteLine($"ТУТ");
+
+        if (direction==0 & col+1 < array2Dim.GetLength(1)) col++; //if (array2Dim[row,col]!=0) col=col-1;
+        if (direction==1 & row+1 < array2Dim.GetLength(0)) row++; //if (array2Dim[row,col]!=0) row=row-1;
+        if (direction==2 & col-1 >=0) col=col-1; // if (array2Dim[row,col]!=0) col++;
+        if (direction==3 & row-1 >=0) row=row-1; // if (array2Dim[row,col]!=0) row++;
+
+        if (array2Dim[row,col]!=0) direction=ChooseDirection(array2Dim, row, col);
+    }   
 }
 
 int ChooseDirection(int[,] array2Dim1, int row, int col)
 {
-        Console.WriteLine($"{row}, {col} -> {array2Dim1[row,col]}");
+        Console.WriteLine($"{row}, {col} -> {array2Dim1[row,col]} choose dir");
         //Console.WriteLine($"{row}, {col-1} -> {array2Dim1[row,col-1]}");
         if (col+1<array2Dim1.GetLength(1))
         {
@@ -67,14 +65,14 @@ int ChooseDirection(int[,] array2Dim1, int row, int col)
        
         if (col-1>=0) 
         { 
-            if (array2Dim1[row,col-1]==0) return 2; //Console.WriteLine($"{2}");
+            if (array2Dim1[row,col-1]==0) return 2;
         }
 
         if (row-1>=0) 
         {
             if (array2Dim1[row-1,col]==0) return 3;
         }
-        return 0;
+        return 1;
 }
 
 void PrintTwoDimIntArray(int[,] twoDimIntArray1)
